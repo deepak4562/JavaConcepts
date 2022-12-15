503386fe09775e71e2234b1db4998a7e9a0f6999
deepak4562 <dkdkdas545@gmail.com>
2022-12-16 00:21:57 +0530

    two classes are added 'Literals.java' and 'ValueFindOut.java'

---
+++ src/Java_Fundamental/Literals.java
@@ -0,0 +1,12 @@
+package Java_Fundamental;
+
+public class Literals {
+    public static void main(String[] args) {
+        char ch=97;
+        System.out.println(ch);
+        char ch2=0xbee;
+        System.out.println(ch2);
+        char ch3=0777;
+        System.out.println(ch3);
+    }
+}
